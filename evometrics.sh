b23cef4145749f6d8dea9deed069f2044d438a27
paulorfarah <paulorfarah@gmail.com>
2022-09-26 20:24:51 -0600

    added scripts to extract evolutionary metrics

---
+++ evometrics.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+check_parameter () {
+	    : ${1?Repo url required.}
+    }
+check_parameter $1
+
+# set target directories
+DIR_A="projectA/"
+DIR_B="projectB/"
+
+# clean target directories
+rm -Rf "$DIR_A"
+rm -Rf "$DIR_B"
+
+#clone repository to target directories
+REPO=$1
+PROJECT_NAME=${REPO##*/}
+DIR_A="${DIR_A}${PROJECT_NAME}"
+DIR_B="${DIR_B}${PROJECT_NAME}"
+
+git clone "$REPO" "$DIR_A"
+git clone "$REPO" "$DIR_B"
+#echo "Script executed from: ${PWD}"
+nohup python3 evometrics.py --pathA "${DIR_A}" --pathB "${DIR_B}" --commits "${PROJECT_NAME}".txt --projectName "${PROJECT_NAME}" --absolutePath "${PWD}/" --mode "csv" &
+#echo "nohop python3 evometrics.py --pathA ${DIR_A} --pathB ${DIR_B} --commits ${PROJECT_NAME}.txt --projectName ${PROJECT_NAME} --absolutePath ${PWD}/ --mode tag &"
