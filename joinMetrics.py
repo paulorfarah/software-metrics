0d244537e53b7db394897140ffd1a39b59f53a60
Paulo R. Farah <paulorfarah@gmail.com>
2022-10-05 10:00:32 -0300

    software metrics working, but understand need to improve index and header label

---
+++ joinMetrics.py
@@ -0,0 +1,285 @@
+import glob
+import os
+from asyncore import write
+from importlib.resources import path
+import pydriller
+import argparse
+from csv import reader
+import csv
+from pydriller import Repository
+import git
+import pandas as pd
+import numpy as np
+
+
+def parse_ck_results(file_path, project_name):
+    #
+    # list all csv files only
+    csv_files = glob.glob(file_path + '*_class.{}'.format('csv'))
+    # print(csv_files)
+    df_all = pd.DataFrame()
+    try:
+        # names = ['project_name', 'commit_hash', 'class_name',
+        #            'PublicFieldCount', 'IsAbstract', 'ClassLinesOfCode',
+        #            'WeighOfClass', 'FANIN', 'TightClassCohesion', 'FANOUT',
+        #            'OverrideRatio', 'LCOM3', 'WeightedMethodCount', 'LCOM2',
+        #            'NumberOfAccessorMethods', 'LazyClass', 'DataClass',
+        #            'ComplexClass', 'SpaghettiCode', 'SpeculativeGenerality',
+        #            'GodClass', 'RefusedBequest', 'ClassDataShouldBePrivate',
+        #            'BrainClass', 'TotalClass', 'LongParameterList', 'LongMethod',
+        #            'FeatureEnvy', 'DispersedCoupling', 'MessageChain',
+        #            'IntensiveCoupling', 'ShotgunSurgery', 'BrainMethod',
+        #            'TotalMethod', 'TotalClassMethod', 'DiversityTotal',
+        #            'DiversityMethod', 'DiversityClass']
+        df_all = pd.read_csv('results/ck/ck_all.csv')  # , dtype={"user_id": int, "username": "string"})
+
+    except:
+        pass
+    for file in csv_files:
+        # print(file)
+        hash = file.split('/')[-1][:-4]
+        hash = hash.replace(project_name + '_', '')
+        hash = hash.replace('_class', '')
+        # print(hash)
+        df1 = pd.read_csv(file)
+        df2 = pd.read_csv(file[:-10] + '_method.csv')
+        # df_temp = df1.append(df2)
+        df_temp = pd.merge(df1, df2, on=['file', 'class'], how='left').reset_index()
+        df_temp['commit_hash'] = hash
+        df_temp['project_name'] = project_name
+        # df_temp.to_csv(file[:-10] + '.csv')
+        df_all = df_all.append(df_temp)
+
+    df_all = df_all.rename(columns={'oldName2': 'newName2'})
+    df_all.to_csv('results/ck/ck_all.csv', index=False)
+
+
+def parse_understand_results(file_path, project_name):
+
+    names = []
+    csv_files = glob.glob(file_path + '*.{}'.format('csv'))
+    df_all = pd.DataFrame()
+    try:
+        df_all = pd.read_csv('results/understand/und_all.csv', index_col=False)
+    except:
+        pass
+    for file in csv_files:
+        hash = file.split('/')[-1][:-4]
+        df = pd.read_csv(file, names=)
+        df['commit_hash'] = hash
+        df['project_name'] = project_name
+        df_all = df_all.append(df)
+    df_all.to_csv('results/understand/und_all.csv')
+
+
+def merge_csv_files(file_path, tool):
+    # list all csv files only
+    csv_files = glob.glob(file_path + '*.{}'.format('csv'))
+    df_append = pd.DataFrame()
+
+    for file in csv_files:
+        # files.append(file)
+        if not '_all.csv' in file:
+            if tool == 'changedistiller':
+                chageDistillerMetrics = ["PROJECT_NAME", "CURRENT_COMMIT", "PREVIOUS_COMMIT", "CLASS_CURRENTCOMMIT",
+                                         "CLASS_PREVIOUSCOMMIT", "STATEMENT_DELETE", "STATEMENT_INSERT",
+                                         "STATEMENT_ORDERING_CHANGE",
+                                         "STATEMENT_PARENT_CHANGE", "STATEMENT_UPDATE", "TOTAL_STATEMENTLEVELCHANGES",
+                                         "PARENT_CLASS_CHANGE", "PARENT_CLASS_DELETE", "PARENT_CLASS_INSERT",
+                                         "CLASS_RENAMING",
+                                         "TOTAL_CLASSDECLARATIONCHANGES", "RETURN_TYPE_CHANGE", "RETURN_TYPE_DELETE",
+                                         "RETURN_TYPE_INSERT", "METHOD_RENAMING", "PARAMETER_DELETE",
+                                         "PARAMETER_INSERT",
+                                         "PARAMETER_ORDERING_CHANGE", "PARAMETER_RENAMING", "PARAMETER_TYPE_CHANGE",
+                                         "TOTAL_METHODDECLARATIONSCHANGES", "ATTRIBUTE_RENAMING",
+                                         "ATTRIBUTE_TYPE_CHANGE",
+                                         "TOTAL_ATTRIBUTEDECLARATIONCHANGES", "ADDING_ATTRIBUTE_MODIFIABILITY",
+                                         "REMOVING_ATTRIBUTE_MODIFIABILITY", "REMOVING_CLASS_DERIVABILITY",
+                                         "REMOVING_METHOD_OVERRIDABILITY", "ADDING_CLASS_DERIVABILITY",
+                                         "ADDING_CLASS_DERIVABILITY2", "ADDING_METHOD_OVERRIDABILITY",
+                                         "TOTAL_DECLARATIONPARTCHANGES", "TOTAL_CHANGES"]
+
+                df_temp = pd.read_csv(file,
+                            names=chageDistillerMetrics, dtype = {"PROJECT_NAME": "string", "CURRENT_COMMIT": "string",
+                                                    "PREVIOUS_COMMIT": "string", "CLASS_CURRENTCOMMIT": "string",
+                                                    "CLASS_PREVIOUSCOMMIT": "string", "STATEMENT_DELETE": int,
+                                                    "STATEMENT_INSERT": int,
+                                                    "STATEMENT_ORDERING_CHANGE": int,
+                                                    "STATEMENT_PARENT_CHANGE": int, "STATEMENT_UPDATE": int,
+                                                    "TOTAL_STATEMENTLEVELCHANGES": int,
+                                                    "PARENT_CLASS_CHANGE": int, "PARENT_CLASS_DELETE": int,
+                                                    "PARENT_CLASS_INSERT": int, "CLASS_RENAMING": int,
+                                                    "TOTAL_CLASSDECLARATIONCHANGES": int, "RETURN_TYPE_CHANGE": int,
+                                                    "RETURN_TYPE_DELETE": int,
+                                                    "RETURN_TYPE_INSERT": int, "METHOD_RENAMING": int,
+                                                    "PARAMETER_DELETE": int, "PARAMETER_INSERT": int,
+                                                    "PARAMETER_ORDERING_CHANGE": int, "PARAMETER_RENAMING": int,
+                                                    "PARAMETER_TYPE_CHANGE": int,
+                                                    "TOTAL_METHODDECLARATIONSCHANGES": int, "ATTRIBUTE_RENAMING": int,
+                                                    "ATTRIBUTE_TYPE_CHANGE": int,
+                                                    "TOTAL_ATTRIBUTEDECLARATIONCHANGES": int,
+                                                    "ADDING_ATTRIBUTE_MODIFIABILITY": int,
+                                                    "REMOVING_ATTRIBUTE_MODIFIABILITY": int,
+                                                    "REMOVING_CLASS_DERIVABILITY": int,
+                                                    "REMOVING_METHOD_OVERRIDABILITY": int,
+                                                    "ADDING_CLASS_DERIVABILITY": int,
+                                                    "ADDING_CLASS_DERIVABILITY2": int,
+                                                    "ADDING_METHOD_OVERRIDABILITY": int,
+                                                    "TOTAL_DECLARATIONPARTCHANGES": int, "TOTAL_CHANGES": int
+                                                    })
+            else:
+                df_temp = pd.read_csv(file, index_col=False)
+            df_append = df_append.append(df_temp)
+    # df_append = pd.concat(map(pd.read_csv, files), ignore_index=True)
+    df_append.to_csv(file_path + tool + '_all.csv', index=False)
+    return df_append
+
+
+if __name__ == "__main__":
+
+    projects = ['commons-bcel', 'commons-csv', 'easymock', 'gson', 'Openfire', 'commons-text', 'commons-io', 'pdfbox', 'dubbo']
+    result_df = pd.DataFrame()
+    for project_name in projects:
+        # # ck
+        # # file, class, commit_hash, project_name
+        # path = 'results/ck/' + project_name + '/'
+        # parse_ck_results(path, project_name)
+
+        # understand
+        # Kind, Name, File
+        path = 'results/understand/' + project_name + '/'
+        parse_understand_results(path, project_name)
+
+        # # refactoring miner
+        # path = 'results/refactoring/' + project_name + '-results-refactoring-metrics.csv'
+        # df = pd.read_csv(path, names=['commit_hash', 'refactoring', 'class_name', 'type', 'source', 'destination'])
+        # df = df[['commit_hash', 'refactoring', 'class_name', 'source', 'destination']]
+        # df_pivot = pd.pivot_table(df, values=['source', 'destination'],
+        #                               index=['commit_hash', 'class_name'],
+        #                               columns='refactoring',
+        #                               aggfunc=np.sum)
+        #
+        # df_pivot['projectName'] = project_name
+        # df_pivot.reset_index(inplace=True)
+        # result_df = result_df.append(df_pivot)
+
+    # # refactoring miner
+    # result_df.reset_index()
+    # result_df.columns = [' '.join(col).strip() for col in result_df.columns.values]
+    # result_df.to_csv('results/refactoring/refactoring_all.csv',
+    #                  index=False) #columns=['commit_hash', 'refactoring', 'file',
+    #                                                                      #'refactoring_type', 'source', 'destination'],
+    #
+    # tools = ['changedistiller', 'organic', 'evometrics']
+    # for tool in tools:
+    #     path = 'results/' + tool + '/'
+    #     result_df = merge_csv_files(path, tool)
+    #     result_df.to_csv(path + tool + '_all.csv', index=False)
+
+################################################# ROGERIO
+# print(result_df.head())
+# #folder with repo: projectA and projectB
+# pathCK = args.project_name #"/Volumes/backup-geni/projects-smells/results/ck/junit4/repo/junit4"
+# csvPathCK = "results/ck/" + args.project_name + "" #"/Volumes/backup-geni/projects-smells/results/ck/junit4/junit4-all/"
+#
+# csvPathUndestand = "/Volumes/backup-geni/projects-smells/results/understand/junit4/"
+# csvPathProcessMetrics = "/Volumes/backup-geni/projects-smells/results/processMetrics/junit4-results-processMetrics.csv"
+# csvPathChangeDistiller = "/Volumes/backup-geni/projects-smells/results/ChangeDistiller/junit4-results.csv"
+# csvOrganic =  "/Volumes/backup-geni/projects-smells/results/organic/junit4.csv"
+# csvResults = "/Volumes/backup-geni/projects-smells/results/junit4-all-releases.csv"
+#
+# ckRepo = pydriller.Git(pathCK)
+# #understandRepo = pydriller.Git(csvPathUndestand)
+# repo = git.Repo(pathCK)
+# tags = repo.tags
+# release = 1
+# #REMOVED FROM CK - "file"
+# ckClassMetricsAll = ["class","type","cbo","wmc","dit","rfc","lcom","tcc","lcc","totalMethodsQty","staticMethodsQty","publicMethodsQty","privateMethodsQty","protectedMethodsQty","defaultMethodsQty","abstractMethodsQty","finalMethodsQty","synchronizedMethodsQty","totalFieldsQty","staticFieldsQty","publicFieldsQty","privateFieldsQty","protectedFieldsQty","defaultFieldsQty","visibleFieldsQty","finalFieldsQty","synchronizedFieldsQty","nosi","loc","returnQty","loopQty","comparisonsQty","tryCatchQty","parenthesizedExpsQty","stringLiteralsQty","numbersQty","assignmentsQty","mathOperationsQty","variablesQty","maxNestedBlocksQty","anonymousClassesQty","innerClassesQty","lambdasQty","uniqueWordsQty","modifiers","logStatementsQty"]
+#
+# understandMetrics = ["Kind","Name","File","AvgCyclomatic","AvgCyclomaticModified","AvgCyclomaticStrict","AvgEssential","AvgLine","AvgLineBlank","AvgLineCode","AvgLineComment","CountClassBase","CountClassCoupled","CountClassDerived","CountDeclClass","CountDeclClassMethod","CountDeclClassVariable","CountDeclFile","CountDeclFunction","CountDeclInstanceMethod","CountDeclInstanceVariable","CountDeclMethod","CountDeclMethodAll","CountDeclMethodDefault","CountDeclMethodPrivate","CountDeclMethodProtected","CountDeclMethodPublic","CountInput","CountLine","CountLineBlank","CountLineCode","CountLineCodeDecl","CountLineCodeExe","CountLineComment","CountOutput","CountPath","CountSemicolon","CountStmt","CountStmtDecl","CountStmtExe","Cyclomatic","CyclomaticModified","CyclomaticStrict","Essential","MaxCyclomatic","MaxCyclomaticModified","MaxCyclomaticStrict","MaxEssential","MaxInheritanceTree","MaxNesting","PercentLackOfCohesion","RatioCommentToCode","SumCyclomatic","SumCyclomaticModified","SumCyclomaticStrict","SumEssential"]
+#
+# #rename "class" to "className", remove "release"
+# processMetrics = ["project","commit","commitprevious","className","BOC","TACH","FCH","LCH","CHO","FRCH","CHD","WCD","WFR","ATAF","LCA","LCD","CSB","CSBS","ACDF"]
+#
+# chageDistillerMetrics = ["PROJECT_NAME", "CURRENT_COMMIT", "PREVIOUS_COMMIT", "CLASS_CURRENTCOMMIT","CLASS_PREVIOUSCOMMIT",
+# 	    		"STATEMENT_DELETE", "STATEMENT_INSERT", "STATEMENT_ORDERING_CHANGE","STATEMENT_PARENT_CHANGE","STATEMENT_UPDATE","TOTAL_STATEMENTLEVELCHANGES",
+# 	    		"PARENT_CLASS_CHANGE", "PARENT_CLASS_DELETE", "PARENT_CLASS_INSERT","CLASS_RENAMING","TOTAL_CLASSDECLARATIONCHANGES",
+# 	    		"RETURN_TYPE_CHANGE","RETURN_TYPE_DELETE","RETURN_TYPE_INSERT","METHOD_RENAMING","PARAMETER_DELETE","PARAMETER_INSERT","PARAMETER_ORDERING_CHANGE","PARAMETER_RENAMING","PARAMETER_TYPE_CHANGE","TOTAL_METHODDECLARATIONSCHANGES",
+# 	    		"ATTRIBUTE_RENAMING","ATTRIBUTE_TYPE_CHANGE","TOTAL_ATTRIBUTEDECLARATIONCHANGES",
+# 	    		"ADDING_ATTRIBUTE_MODIFIABILITY","REMOVING_ATTRIBUTE_MODIFIABILITY","REMOVING_CLASS_DERIVABILITY","REMOVING_METHOD_OVERRIDABILITY","ADDING_CLASS_DERIVABILITY","ADDING_CLASS_DERIVABILITY","ADDING_METHOD_OVERRIDABILITY", "TOTAL_DECLARATIONPARTCHANGES","TOTAL_CHANGES"]
+# organicMetrics = ["projectName","commitNumber","fullyQualifiedName",
+#                 "PublicFieldCount","IsAbstract","ClassLinesOfCode","WeighOfClass",
+#                 "FANIN","TightClassCohesion","FANOUT","OverrideRatio","LCOM3",
+#                 "WeightedMethodCount","LCOM2","NumberOfAccessorMethods",
+#                 'LazyClass', 'DataClass', 'ComplexClass', 'SpaghettiCode',
+#                 'SpeculativeGenerality', 'GodClass', 'RefusedBequest',
+#                 'ClassDataShouldBePrivate', 'BrainClass', 'TotalClass',
+#                 'LongParameterList', 'LongMethod', 'FeatureEnvy',
+#                     'DispersedCoupling', 'MessageChain', 'IntensiveCoupling',
+#                     'ShotgunSurgery', 'BrainMethod', 'TotalMethod', 'TotalClassMethod',
+#                      "DiversityTotal","DiversityMethod","DiversityClass"]
+# #f = open(csvPath, "w")
+# #writer = csv.writer(f)
+# missing = []
+# for tag in tags:
+#     hashCurrent = ckRepo.get_commit_from_tag(tag.name).hash
+#
+#     try:
+#
+#
+#         releaseUnderstand = pd.read_csv(csvPathUndestand + hashCurrent+'.csv', usecols=understandMetrics, sep=',',engine='python', index_col=False)
+#
+#         print("Understand ")
+#         print(releaseUnderstand.shape[0])
+#
+#         releaseCK = pd.read_csv(csvPathCK + hashCurrent+'-class.csv', usecols=ckClassMetricsAll, sep=',', index_col=False)
+#
+#         print("CK ")
+#         print(releaseCK.shape[0])
+#
+#
+#         releaseOrganicMetrics = pd.read_csv(csvOrganic, usecols=organicMetrics, sep=',',engine='python', index_col=False)
+#         releaseOrganicMetrics = releaseOrganicMetrics[(releaseOrganicMetrics['commitNumber'] == hashCurrent)]
+#
+#         print("Organic ")
+#         print(releaseOrganicMetrics.shape[0])
+#
+#         releaseChangeDistillerMetrics = pd.read_csv(csvPathChangeDistiller, usecols=chageDistillerMetrics, sep=',',engine='python', index_col=False)
+#         releaseChangeDistillerMetrics = releaseChangeDistillerMetrics[(releaseChangeDistillerMetrics['CURRENT_COMMIT'] == hashCurrent)]
+#
+#
+#         print("Change distiller ")
+#         print(releaseChangeDistillerMetrics.shape[0])
+#
+#         releaseProcessMetrics = pd.read_csv(csvPathProcessMetrics, usecols=processMetrics, sep=',', engine='python', index_col=False)
+#         releaseProcessMetrics = releaseProcessMetrics[(releaseProcessMetrics['commit'] == hashCurrent)]
+#
+#         print("Process ")
+#         print(releaseProcessMetrics.shape[0])
+#
+#
+#         #para cada release procurar as classes correspondentes e agregar em um só dataframe se "name" = "class"
+#         ck_understand = pd.merge(left=releaseCK, right=releaseUnderstand, left_on='class', right_on='Name')
+#         ck_understand_process = pd.merge(left=ck_understand, right=releaseProcessMetrics, left_on='class', right_on='className')
+#         ck_understand_process_organic = pd.merge(left=ck_understand_process, right=releaseOrganicMetrics, left_on='class', right_on='fullyQualifiedName')
+#
+#         merged_full = pd.merge(left=ck_understand_process_organic, right=releaseChangeDistillerMetrics, left_on='class', right_on='CLASS_PREVIOUSCOMMIT')
+#
+#         #merged_full.loc[:,'class_frequency'] = 1
+#         merged_full.loc[:,'will_change'] = 0
+#         #merged_full.loc[:,'number_of_changes'] = 0
+#         merged_full.loc[:,'release'] = release
+#         medianChanges = merged_full['TOTAL_CHANGES'].median()
+#         merged_full['will_change'] = np.where(merged_full['TOTAL_CHANGES'] > medianChanges, 1,0)
+#         if(release == 1):
+#             merged_full.to_csv(csvResults, index=False)
+#         else:
+#              merged_full.to_csv(csvResults,mode="a", header=False, index=False)
+#
+#         release += 1
+#     except Exception as e:
+#         print(e)
+#        # print(hashCurrent)
+#         missing.append(hashCurrent)
+#
+# print(missing)
---
+++ refactoring.py
@@ -0,0 +1,172 @@
+from asyncore import write
+import pydriller
+import argparse
+from csv import reader
+import csv
+from pydriller import Repository
+import git
+import os
+import json
+
+
+def parse_json(path, refactoring_dict, commit_sha1):
+    with open(path, 'r') as json_file:
+        json_data = json.load(json_file)
+
+        for json_commit in json_data["commits"]:
+            # commit_sha1 = json_commit['sha1']
+            # refactoring_dict = {commit_sha1: {}}
+            for json_refactoring in json_commit["refactorings"]:
+                print(json_refactoring["type"] + ' (parse_json)')
+                refactoring_type = json_refactoring["type"]
+                if refactoring_type not in refactoring_dict[commit_sha1]:
+                    refactoring_dict[commit_sha1][refactoring_type] = {}
+
+                for json_left in json_refactoring["leftSideLocations"]:
+                    file_path = json_left["filePath"]
+                    code_element_type = json_left["codeElementType"]
+                    code_element = json_left["codeElement"]
+                    if file_path not in refactoring_dict[commit_sha1][refactoring_type]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path] = {}
+
+                    if code_element_type not in refactoring_dict[commit_sha1][refactoring_type][file_path]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type] = {'source': 1}
+                    elif 'source' not in refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]['source'] = 1
+                    else:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]['source'] += 1
+                    # else:
+                    #     refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]['source'] += 1
+
+                for json_left in json_refactoring["rightSideLocations"]:
+                    file_path = json_left["filePath"]
+                    code_element_type = json_left["codeElementType"]
+                    code_element = json_left["codeElement"]
+                    if file_path not in refactoring_dict[commit_sha1][refactoring_type]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path] = {}
+
+                    if code_element_type not in refactoring_dict[commit_sha1][refactoring_type][file_path]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type] = {
+                            'destination': 1}
+                    elif 'destination' not in refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]['destination'] = 1
+                    else:
+                        refactoring_dict[commit_sha1][refactoring_type][file_path][code_element_type]['destination'] += 1
+    return refactoring_dict
+
+def convertJson(writer, ref_dict, commit_sha1):
+    for type_key, type_val in ref_dict[commit_sha1].items():
+        if isinstance(type_val, dict):
+            for file_key, file_val in type_val.items():
+                if isinstance(file_val, dict):
+                    for elem_type_key, elem_type_val in file_val.items():
+                        source = 0
+                        destination = 0
+                        if isinstance(elem_type_val, dict):
+                            if 'source' in elem_type_val:
+                                source = elem_type_val['source']
+                            if 'destination' in elem_type_val:
+                                destination = elem_type_val['destination']
+                        row = [commit_sha1, type_key, file_key, elem_type_key, source, destination]
+                        writer.writerow(row)
+                        f.flush()
+                else:
+                    row = [commit_sha1, type_key, file_key, file_val, 0, 0]
+                    writer.writerow(row)
+                    f.flush()
+        else:
+            print('>>> Not a dictionary: ' + type_key)
+
+
+if __name__ == "__main__":
+
+    print('starting refactoring miner...')
+    ap = argparse.ArgumentParser(description='Extract refactorings')
+    ap.add_argument('--project_name', required=True)
+    ap.add_argument('--git_repo_folder_A', required=True)
+    ap.add_argument('--git_repo_folder_B', required=True)
+    ap.add_argument('--mode', required=True, help='mode - tag for commits with tag, csv - for csv of commits')
+    args = ap.parse_args()
+
+    # folder with repo: projectA and projectB
+    pathA = pydriller.Git(args.git_repo_folder_A)
+    pathB = pydriller.Git(args.git_repo_folder_B)
+
+    release = 1
+    tag_previous = ''
+    refactoring_list = ["Extract Method", "Inline Method", "Rename Method", "Move Method", "Move Attribute",
+                        "Pull Up Method", "Pull Up Attribute", "Push Down Method", "Push Down Attribute",
+                        "Extract Superclass", "Extract Interface", "Move Class", "Rename Class",
+                        "Extract and Move Method", "Rename Package", "Move and Rename Class", "Extract Class",
+                        "Extract Subclass", "Extract Variable", "Inline Variable", "Parameterize Variable",
+                        "Rename Variable", "Rename Parameter", "Rename Attribute", "Move and Rename Attribute",
+                        "Replace Variable with Attribute", "Replace Attribute (with Attribute)", "Merge Variable",
+                        "Merge Parameter", "Merge Attribute", "Split Variable", "Split Parameter", "Split Attribute",
+                        "Change Variable Type", "Change Parameter Type", "Change Return Type", "Change Attribute Type",
+                        "Extract Attribute", "Move and Rename Method", "Move and Inline Method",
+                        "Add Method Annotation",
+                        "Remove Method Annotation", "Modify Method Annotation", "Add Attribute Annotation",
+                        "Remove Attribute Annotation",
+                        "Modify Attribute Annotation", "Add Class Annotation", "Remove Class Annotation",
+                        "Modify Class Annotation", "Add Parameter Annotation", "Remove Parameter Annotation",
+                        "Modify Parameter Annotation", "Add Variable Annotation", "Remove Variable Annotation",
+                        "Modify Variable Annotation", "Add Parameter", "Remove Parameter", "Reorder Parameter",
+                        "Add Thrown Exception Type", "Remove Thrown Exception Type", "Change Thrown Exception Type",
+                        "Change Method Access Modifier", "Change Attribute Access Modifier", "Encapsulate Attribute",
+                        "Parameterize Attribute", "Replace Attribute with Variable",
+                        "Add Method Modifier (final, static, abstract, synchronized)",
+                        "Remove Method Modifier (final, static, abstract, synchronized)",
+                        "Add Attribute Modifier (final, static, transient, volatile)",
+                        "Remove Attribute Modifier (final, static, transient, volatile)",
+                        "Add Variable Modifier (final)", "Add Parameter Modifier (final)",
+                        "Remove Variable Modifier (final)", "Remove Parameter Modifier (final)",
+                        "Change Class Access Modifier",
+                        "Add Class Modifier (final, static, abstract)",
+                        "Remove Class Modifier (final, static, abstract)",
+                        "Move Package", "Split Package", "Merge Package", "Localize Parameter",
+                        "Change Type Declaration Kind (class, interface, enum)", "Collapse Hierarchy",
+                        "Replace Loop with Pipeline", "Replace Anonymous with Lambda", "Merge Class",
+                        "Inline Attribute",
+                        "Replace Pipeline with Loop", "Split Class", "Split Conditional"]
+
+    # class_dict = {}
+
+    csvPath = "results/refactoring/" + args.project_name + "-results-refactoring-metrics.csv"
+    with open(csvPath, "w") as f:
+        writer = csv.writer(f)
+
+        if (args.mode == 'tag'):
+            repo = git.Repo(args.git_repo_folder_A)
+            tags = repo.tags
+
+            for tag_current in tags:
+                hashCurrent = pathB.get_commit_from_tag(tag_current.name).hash
+                print('######################## hashCurrent #############################', hashCurrent)
+                pathA.checkout(hashCurrent)
+                refactoring_dict = {hashCurrent: {}}
+                # rows = []
+                if release == 1:
+                    tag_previous = tag_current
+                    row = ['commit', 'refactoring', 'filePath', 'codeElementType', 'source', 'destination']# + refactoring_list
+                else:
+                    hashPrevious = pathA.get_commit_from_tag(tag_previous.name).hash
+                    pathB.checkout(hashPrevious)
+                    commits_range = Repository(args.git_repo_folder_A, from_commit=hashPrevious, to_commit=hashCurrent).traverse_commits()
+                    for cc in commits_range:
+                        print(cc.hash)
+                        path_to_json_file = 'results/refactoring/json/' + args.project_name + '_' + cc.hash + '-refactoringminer.json'
+                        out = os.popen(
+                            'RefactoringMiner-2.3.2/bin/RefactoringMiner -c ' + args.git_repo_folder_A + ' ' + cc.hash + ' -json ' + path_to_json_file).read()
+                        refactoring_dict = parse_json(path_to_json_file, refactoring_dict, hashCurrent)
+
+                    convertJson(writer, refactoring_dict, hashCurrent)
+                # for row in rows:
+
+
+                tag_previous = tag_current
+                release += 1
+
+        else:
+            print('ATTENTION: mode not implemented.')
+
+    print(args.project_name + ' ended.')
