9f87ca6c2860ca155e5f517dba4bc645812bf952
Paulo R. Farah <paulorfarah@gmail.com>
2022-10-15 18:52:30 -0300

    dropping columns with variability lower than 1 working

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,39 +3,52 @@ import pandas as pd
 from numpy import loadtxt, percentile, nan
 from numpy import unique
 # load the dataset
-df = pd.read_csv('results/ck_all.csv')
 
-# summarize the dataset
-df.describe().to_csv('ck_describe.csv')
+ignored_cols = {'ck_100': ['file', 'class', 'type', 'method', 'constructor', 'commit_hash', 'project_name']}
 
-# summarize the number of unique values in each column (1%)
-print(df.shape[1])
-# Writing to file
-with open("unique.csv", "w") as file1:
-    # Writing data to a file
-    # file1.write("Hello \n")
-    # file1.writelines(L)
+# metrics = ['ck', 'evometrics', 'organic', 'refactoring', 'und', 'changedistiller']
+# for metric in metrics:
+#     df = pd.read_csv('results/' + metric + '_all.csv')
+local = True
+if local:
+    metric = 'ck_100'
+    df = pd.read_csv('results/ck_all_100.csv')
+    # summarize the dataset
+    df.describe().to_csv(metric + '_describe.csv')
 
-    df_class = df[:, 'cbo_x':'logStatementsQty_x']
-    print(df_class.head())
-    for i in range(df_class.shape[1]):
-        num = len(unique(df.iloc[:, i]))
-        percentage = float(num) / df.shape[0] * 100
-        if percentage < 1:
-            # print('%d, %d, %.1f%%' % (i, num, percentage))
-            file1.write('%d, %d, %.1f%%' % (i, num, percentage))
+    # summarize the number of unique values in each column (1%)
+    # print(df.shape[1])
+    # Writing to file
+    with open("results/variability_" + metric + ".csv", "w") as file1:
+        # Writing data to a file
+        # file1.write("Hello \n")
+        # file1.writelines(L)
 
-#
-# # removing columns with low variance
-# # get number of unique values for each column
-# counts = df.nunique()
-# # record columns to delete
-# to_del = [i for i, v in enumerate(counts) if (float(v)/df.shape[0]*100) < 1]
-# print(to_del)
-# # drop useless columns
-# df.drop(to_del, axis=1, inplace=True)
-# df.to_csv('ck_drop.csv')
-#
+        # df_class = df[:, 'cbo_x':'logStatementsQty_x']
+        # print(df_class.head())
+        for i in range(df.shape[1]):
+            if not df.columns[i] in ignored_cols[metric]:
+                num = len(unique(df.iloc[:, i]))
+                percentage = float(num) / df.shape[0] * 100
+                if percentage < 1.0:
+                    print('%s, %d, %.2f%%' % (df.columns[i], num, percentage))
+                    file1.write('%s, %d, %.1f%%\n' % (df.columns[i], num, percentage))
+
+
+
+    # removing columns with low variance
+    # get number of unique values for each column
+    counts = df.nunique()
+    # record columns to delete
+    to_del = [df.columns[i] for i, v in enumerate(counts) if ((float(v)/df.shape[0]*100) < 1) and (df.columns[i] not in ignored_cols[metric])]
+
+    with open("results/" + metric + "_clean.csv", "w") as file1:
+        for col in to_del:
+            file1.write(col + '\n')
+    # drop useless columns
+    df.drop(to_del, axis=1, inplace=True)
+    df.to_csv('results/drop_' + metric + '.csv')
+
 # def evaluate_features_threshold():
 #     # split data into inputs and outputs
 #     data = df.values
@@ -93,3 +106,5 @@ with open("unique.csv", "w") as file1:
 #     # count the number of nan values in each column
 #     print(df.isnull().sum())
 #     return df
+
+print('end')
