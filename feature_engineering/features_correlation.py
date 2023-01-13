e8496261920e6efffccd5f45f667f5ae1df2a20e
Paulo R. Farah <paulorfarah@gmail.com>
2023-01-13 16:06:39 -0300

    implemented correlation to all systems

--- feature_engineering/features_correlation.py
+++ feature_engineering/features_correlation.py
@@ -3,12 +3,13 @@ import sys
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+from numpy import percentile
 
 pd.set_option('display.max_columns', None)
 
 
-def join_all_dataset(project_name):
-    print(project_name)
+def ck_ownduration_correlation(project_name):
+    print(project_name + ' ck')
     df_pd = pd.read_csv('../dynamic_metrics/results/' + project_name + '-performance-diff.csv', sep=',')  # ok
     df_pd['project_name'] = project_name
     df = pd.DataFrame().assign(project_name=df_pd['project_name'], commit_hash=df_pd['commit'],
@@ -34,8 +35,10 @@ def join_all_dataset(project_name):
                   'hasJavaDoc', 'commit_hash', 'project_name']
     df_ck = pd.read_csv('../static_metrics/results/ck/ck_all.csv', usecols=ck_metrics, sep=',',
                         index_col=False)
-    df_ck = df_ck.loc[df['project_name'] == project_name]
-    path1 = '/mnt/sda4/software-metrics/static_metrics/' + project_name + '/'
+    df_ck = df_ck.loc[df_ck['project_name'] == project_name]
+    path1 = '/groups/ilabt-imec-be/software-performance/ck/' + project_name + '/'
+
+
     df_ck['file'] = df_ck['file'].str.replace(path1, '')
     try:
         spl_word = "commons-"
@@ -51,12 +54,251 @@ def join_all_dataset(project_name):
     #plot correlations
     metrics = ['own_duration'] + ck_metrics[4:52] + ck_metrics[55:83]
 
+
+    q25, q75 = percentile(df['own_duration'], 25), percentile(df['own_duration'], 75)
+    iqr = q75 - q25
+    cut_off = iqr * 1.5
+    lower, upper = q25 - cut_off, q75 + cut_off
+    # identify outliers
+    outliers = [x for x in df['own_duration'] if x < lower or x > upper]
+    print('outliers:')
+    for o in outliers:
+        print(o)
+
+    # df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+    df_outliers = df.loc[(df['own_duration'] >= lower) & (df['own_duration'] <= upper)]
     for m in metrics:
-        sns.lmplot(x="own_duration", y=m, data=df)
-        plt.savefig('results/correlation/ck/' + m + '.pdf')
+        sns.lmplot(x="own_duration", y=m, data=df_outliers)
+        plt.savefig('results/correlation/ck/' + project_name + '/' + project_name + '_' + m + '2.pdf')
     # plt.show()
 
-    df[metrics].corr().to_csv('results/correlation/ck_correlation.csv')
+    df[metrics].corr().to_csv('results/correlation/' + project_name + 'ck_correlation.csv', index=False)
+    df_outliers[metrics].corr().to_csv('results/correlation/' + project_name + 'ck_correlation_outliers.csv', index=False)
+
+
+def und_ownduration_correlation(project_name):
+    print(project_name + ' und')
+    df_pd = pd.read_csv('../dynamic_metrics/results/' + project_name + '-performance-diff.csv', sep=',')  # ok
+    df_pd['project_name'] = project_name
+    df = pd.DataFrame().assign(project_name=df_pd['project_name'], commit_hash=df_pd['commit'],
+                               class_name=df_pd['class_name'], own_duration=df_pd['avg1'])
+    df = df.merge(df_pd[['project_name', 'prevcommit', 'class_name', 'avg2']].rename(
+        columns={'prevcommit': 'commit_hash', 'avg2': 'own_duration'}), how='outer')
+
+    # understand
+    und_metrics = ["index1", "index2", "index3", "index4", "index5", "index6", "index7", "Kind", "Name", "File",
+    # und_metrics = ["Name", "File",
+                   "AvgCyclomatic", "AvgCyclomaticModified", "AvgCyclomaticStrict",
+                   "AvgEssential", "AvgLine", "AvgLineBlank", "AvgLineCode", "AvgLineComment", "CountClassBase",
+                   "CountClassCoupled", "CountClassDerived", "CountDeclClass", "CountDeclClassMethod",
+                   "CountDeclClassVariable", "CountDeclFile", "CountDeclFunction", "CountDeclInstanceMethod",
+                   "CountDeclInstanceVariable", "CountDeclMethod", "CountDeclMethodAll",
+                   "CountDeclMethodDefault", "CountDeclMethodPrivate", "CountDeclMethodProtected",
+                   "CountDeclMethodPublic", "CountInput", "CountLine", "CountLineBlank", "CountLineCode",
+                   "CountLineCodeDecl", "CountLineCodeExe", "CountLineComment", "CountOutput", "CountPath",
+                   "CountSemicolon", "CountStmt", "CountStmtDecl", "CountStmtExe", "Cyclomatic",
+                   "CyclomaticModified", "CyclomaticStrict", "Essential", "MaxCyclomatic",
+                   "MaxCyclomaticModified", "MaxCyclomaticStrict", "MaxEssential", "MaxInheritanceTree",
+                   "MaxNesting", "PercentLackOfCohesion", "RatioCommentToCode", "SumCyclomatic",
+                   "SumCyclomaticModified", "SumCyclomaticStrict", "SumEssential", 'unknown', 'commit_hash',
+                   'project_name']
+    df_und = pd.read_csv('../static_metrics/results/und/und_all.csv', sep=',',
+                         engine='python', names=und_metrics)
+
+    df_und = df_und.loc[df_und['project_name'] == project_name]
+    df_und = df_und[df_und.columns[8:]]
+    df_und['class'] = df_und['Name']
+
+    df = pd.merge(left=df, right=df_und, left_on=['project_name', 'commit_hash', 'file'],
+                  right_on=['project_name', 'commit_hash', 'File'], how='left')
+
+    #plot correlations
+    metrics = ['own_duration'] + und_metrics[2:55]
+
+    q25, q75 = percentile(df['own_duration'], 25), percentile(df['own_duration'], 75)
+    iqr = q75 - q25
+    cut_off = iqr * 1.5
+    lower, upper = q25 - cut_off, q75 + cut_off
+    # identify outliers
+    outliers = [x for x in df['own_duration'] if x < lower or x > upper]
+    print('outliers:')
+    for o in outliers:
+        print(o)
+
+    # df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+    df_outliers = df.loc[(df['own_duration'] >= lower) & (df['own_duration'] <= upper)]
+    for m in metrics:
+        sns.lmplot(x="own_duration", y=m, data=df_outliers)
+        plt.savefig('results/correlation/und/' + project_name + '/' + project_name + '_' + m + '-outliers.pdf')
+    # plt.show()
+
+    df[metrics].corr().to_csv('results/correlation/' + project_name + 'und_correlation.csv', index=False)
+    df_outliers[metrics].corr().to_csv('results/correlation/' + project_name + 'und_correlation_outliers.csv', index=False)
+
+
+# def evo_ownduration_correlation(project_name):
+#     print(project_name)
+#     df_pd = pd.read_csv('../dynamic_metrics/results/' + project_name + '-performance-diff.csv', sep=',')  # ok
+#     df_pd['project_name'] = project_name
+#     df = pd.DataFrame().assign(project_name=df_pd['project_name'], commit_hash=df_pd['commit'],
+#                                class_name=df_pd['class_name'], own_duration=df_pd['avg1'])
+#     df = df.merge(df_pd[['project_name', 'prevcommit', 'class_name', 'avg2']].rename(
+#         columns={'prevcommit': 'commit_hash', 'avg2': 'own_duration'}), how='outer')
+#
+#     # evolutionary metrics
+#     evo_metrics = ["project", "commit", "commitprevious", "class", "BOC", "TACH", "FCH", "LCH", "CHO", "FRCH",
+#                    "CHD", "WCD", "WFR", "ATAF", "LCA", "LCD", "CSB", "CSBS", "ACDF"]
+#     df_evo = pd.read_csv('../static_metrics/results/evometrics/evometrics_all.csv',
+#                          usecols=evo_metrics, sep=',', index_col=False)
+#     path1 = '/home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/'
+#     # /home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/src/examples/listclass.java
+#
+#     df_evo['file'] = df_evo['class'].str.replace(path1, '')
+#     df = pd.merge(left=df, right=df_evo, left_on=['project_name', 'commit_hash', 'class_name'],
+#                   right_on=['project', 'commit', 'file'], how='left')
+#
+#     #plot correlations
+#     metrics = ['own_duration'] + evo_metrics[8:23]
+#
+#     q25, q75 = percentile(df['own_duration'], 25), percentile(df['own_duration'], 75)
+#     iqr = q75 - q25
+#     cut_off = iqr * 1.5
+#     lower, upper = q25 - cut_off, q75 + cut_off
+#     # identify outliers
+#     outliers = [x for x in df['own_duration'] if x < lower or x > upper]
+#     print('outliers:')
+#     for o in outliers:
+#         print(o)
+#
+#     # df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+#     df_outliers = df.loc[(df['own_duration'] >= lower) & (df['own_duration'] <= upper)]
+#     for m in metrics:
+#         sns.lmplot(x="own_duration", y=m, data=df_outliers)
+#         plt.savefig('results/correlation/evo/' + project_name + '_' + m + '-outliers.pdf')
+#     # plt.show()
+#
+#     df[metrics].corr().to_csv('results/correlation/' + project_name + 'evo_correlation.csv', index=False)
+#     df_outliers[metrics].corr().to_csv('results/correlation/' + project_name + 'evo_correlation_outliers.csv', index=False)
+
+
+def evo_ownduration_correlation(project_name):
+    print(project_name + ' evo')
+    df_pd = pd.read_csv('../dynamic_metrics/results/' + project_name + '-performance-diff.csv', sep=',')  # ok
+    df_pd['project_name'] = project_name
+    df = pd.DataFrame().assign(project_name=df_pd['project_name'], commit_hash=df_pd['commit'],
+                               class_name=df_pd['class_name'], own_duration=df_pd['avg1'])
+    df = df.merge(df_pd[['project_name', 'prevcommit', 'class_name', 'avg2']].rename(
+        columns={'prevcommit': 'commit_hash', 'avg2': 'own_duration'}), how='outer')
+
+    # evolutionary metrics
+    evo_metrics = ["project", "commit", "commitprevious", "class", "BOC", "TACH", "FCH", "LCH", "CHO", "FRCH",
+                   "CHD", "WCD", "WFR", "ATAF", "LCA", "LCD", "CSB", "CSBS", "ACDF"]
+    df_evo = pd.read_csv('../static_metrics/results/evometrics/evometrics_all.csv',
+                         usecols=evo_metrics, sep=',', index_col=False)
+    df_evo = df_evo.loc[df_evo['project'] == project_name]
+
+    path1 = '/home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/' + project_name + '/'
+    # /home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/src/examples/listclass.java
+    # /home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/src/examples/listclass.java
+    df_evo['file'] = df_evo['class'].str.replace(path1, '')
+    df = pd.merge(left=df, right=df_evo, left_on=['project_name', 'commit_hash', 'class_name'],
+                  right_on=['project', 'commit', 'file'], how='left')
+
+    #plot correlations
+    metrics = ['own_duration'] + evo_metrics[8:23]
+
+    q25, q75 = percentile(df['own_duration'], 25), percentile(df['own_duration'], 75)
+    iqr = q75 - q25
+    cut_off = iqr * 1.5
+    lower, upper = q25 - cut_off, q75 + cut_off
+    # identify outliers
+    outliers = [x for x in df['own_duration'] if x < lower or x > upper]
+    print('outliers:')
+    for o in outliers:
+        print(o)
+
+    # df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+    df_outliers = df.loc[(df['own_duration'] >= lower) & (df['own_duration'] <= upper)]
+    for m in metrics:
+        sns.lmplot(x="own_duration", y=m, data=df_outliers)
+        plt.savefig('results/correlation/evo/' + project_name + '/' + project_name + '_' + m + '-outliers.pdf')
+    # plt.show()
+
+    df[metrics].corr().to_csv('results/correlation/' + project_name + 'evo_correlation.csv', index=False)
+    df_outliers[metrics].corr().to_csv('results/correlation/' + project_name + 'evo_correlation_outliers.csv', index=False)
+
+def cd_ownduration_correlation(project_name):
+    print(project_name + ' cd')
+    df_pd = pd.read_csv('../dynamic_metrics/results/' + project_name + '-performance-diff.csv', sep=',')  # ok
+    df_pd['project_name'] = project_name
+    df = pd.DataFrame().assign(project_name=df_pd['project_name'], commit_hash=df_pd['commit'],
+                               class_name=df_pd['class_name'], own_duration=df_pd['avg1'])
+    df = df.merge(df_pd[['project_name', 'prevcommit', 'class_name', 'avg2']].rename(
+        columns={'prevcommit': 'commit_hash', 'avg2': 'own_duration'}), how='outer')
+
+    # # evolutionary metrics
+    # evo_metrics = ["project", "commit", "commitprevious", "class", "BOC", "TACH", "FCH", "LCH", "CHO", "FRCH",
+    #                "CHD", "WCD", "WFR", "ATAF", "LCA", "LCD", "CSB", "CSBS", "ACDF"]
+    # df_evo = pd.read_csv('../static_metrics/results/evometrics/evometrics_all.csv',
+    #                      usecols=evo_metrics, sep=',', index_col=False)
+    # path1 = '/home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/'
+    # # /home/usuario/OneDrive/ufpr/datasets/software-metrics/projectA/commons-bcel/src/examples/listclass.java
+    #
+    # df_evo['file'] = df_evo['class'].str.replace(path1, '')
+    # df = pd.merge(left=df, right=df_evo, left_on=['project_name', 'commit_hash', 'class_name'],
+    #               right_on=['project', 'commit', 'file'], how='left')
+
+    changedistiller_metrics = ["PROJECT_NAME", "CURRENT_COMMIT", "PREVIOUS_COMMIT", "CLASS_CURRENTCOMMIT",
+                               "CLASS_PREVIOUSCOMMIT",
+                               "STATEMENT_DELETE", "STATEMENT_INSERT", "STATEMENT_ORDERING_CHANGE",
+                               "STATEMENT_PARENT_CHANGE", "STATEMENT_UPDATE", "TOTAL_STATEMENTLEVELCHANGES",
+                               "PARENT_CLASS_CHANGE", "PARENT_CLASS_DELETE", "PARENT_CLASS_INSERT", "CLASS_RENAMING",
+                               "TOTAL_CLASSDECLARATIONCHANGES",
+                               "RETURN_TYPE_CHANGE", "RETURN_TYPE_DELETE", "RETURN_TYPE_INSERT", "METHOD_RENAMING",
+                               "PARAMETER_DELETE", "PARAMETER_INSERT", "PARAMETER_ORDERING_CHANGE",
+                               "PARAMETER_RENAMING",
+                               "PARAMETER_TYPE_CHANGE", "TOTAL_METHODDECLARATIONSCHANGES",
+                               "ATTRIBUTE_RENAMING", "ATTRIBUTE_TYPE_CHANGE", "TOTAL_ATTRIBUTEDECLARATIONCHANGES",
+                               "ADDING_ATTRIBUTE_MODIFIABILITY", "REMOVING_ATTRIBUTE_MODIFIABILITY",
+                               "REMOVING_CLASS_DERIVABILITY", "REMOVING_METHOD_OVERRIDABILITY",
+                               "ADDING_CLASS_DERIVABILITY", "ADDING_CLASS_DERIVABILITY", "ADDING_METHOD_OVERRIDABILITY",
+                               "TOTAL_DECLARATIONPARTCHANGES", "TOTAL_CHANGES"]
+    df_cd = pd.read_csv('../static_metrics/results/changedistiller/changedistiller_all.csv',
+                        sep=',', index_col=False)
+    df_cd.columns = changedistiller_metrics
+    df_cd = df_cd.loc[df_cd['PROJECT_NAME'] == project_name]
+    # / groups / ilabt - imec - be / software - performance / changedistiller / projectB / commons - bcel / examples / HelloWorldBuilder.java
+    # / groups / ilabt - imec - be / software - performance / changedistiller / projectB / commons - bcel / examples / HelloWorldBuilder.java
+
+    path1 = '/groups/ilabt-imec-be/software-performance/changedistiller/projectB/' + project_name + '/'
+    df_cd['file'] = df_cd['CLASS_PREVIOUSCOMMIT'].str.replace(path1, '')
+    df = pd.merge(left=df, right=df_cd, left_on=['project_name', 'commit_hash', 'class_name'],
+                  right_on=['PROJECT_NAME', 'PREVIOUS_COMMIT', 'file'], how='left')
+
+    #plot correlations
+    metrics = ['own_duration'] + changedistiller_metrics[9:41]
+
+    q25, q75 = percentile(df['own_duration'], 25), percentile(df['own_duration'], 75)
+    iqr = q75 - q25
+    cut_off = iqr * 1.5
+    lower, upper = q25 - cut_off, q75 + cut_off
+    # identify outliers
+    outliers = [x for x in df['own_duration'] if x < lower or x > upper]
+    print('outliers:')
+    for o in outliers:
+        print(o)
+
+    # df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
+    df_outliers = df.loc[(df['own_duration'] >= lower) & (df['own_duration'] <= upper)]
+    for m in metrics:
+        print(m)
+        if m != 'ADDING_CLASS_DERIVABILITY':
+            sns.lmplot(x="own_duration", y=m, data=df_outliers)
+            plt.savefig('results/correlation/evo/' + project_name + '/' + project_name + '_' + m + '-outliers.pdf')
+    # plt.show()
+
+    df[metrics].corr().to_csv('results/correlation/' + project_name + '_cd_correlation.csv', index=False)
+    df_outliers[metrics].corr().to_csv('results/correlation/' + project_name + '_cd_correlation_outliers.csv', index=False)
 
 
 def join_jgit_dataset(project_name, commits_list):
@@ -285,6 +527,7 @@ def join_jgit_dataset(project_name, commits_list):
     # plt.show()
 
     df[metrics].corr().to_csv('results/correlation/cd_correlation.csv')
+    return df
 
 if __name__ == "__main__":
 
@@ -304,7 +547,15 @@ if __name__ == "__main__":
                         '4f0daa3bb2a5fa28286f1973deb9d13996cc73cc', 'bf32c9102fb1b5fdfa7a26a120b5d9a6b428dd2f',
                         '29159f1171c4930128ab0557836e856ce8cba6c8'],
                }
-    # for project_name in projects:
-    #     join_jgit_dataset(project_name, commits[project_name])
 
-    join_all_dataset('bcel')
+    # jgit_all = []
+    # for project_name in projects:
+    #     jgit_all.append(join_jgit_dataset(project_name, commits[project_name]))
+    # print(jgit_all['project_name'].unique())
+
+    systems = ['commons-text', 'easymock', 'openfire']
+    for system in systems:
+        ck_ownduration_correlation(system)
+        # und_ownduration_correlation(system)
+        evo_ownduration_correlation(system)
+        cd_ownduration_correlation(system)
