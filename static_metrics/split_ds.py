a270391a1eeea086280eae9545abafdc8a3dfbec
Paulo R. Farah <paulorfarah@gmail.com>
2023-01-20 15:49:54 -0300

    script to split dataset by project

---
+++ static_metrics/split_ds.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('results/ck/ck_all.csv')
+
+projects = ['commons-bcel', 'commons-text', 'commons-csv', 'easymock', 'jgit', 'gson', 'openfire']
+
+for prj in projects:
+    dft = df.loc[df['project_name'] == prj]
+    dft.to_csv('results/ck/' + prj + 'ck_2.csv', index=False)
