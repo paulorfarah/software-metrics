f314eb8501b8cb4fb1a2d041118c64e02a881263
Paulo Roberto Farah <paulorfarah@gmail.com>
2023-01-21 05:50:28 -0700

    updated split_ds to run on geni

--- static_metrics/split_ds.py
+++ static_metrics/split_ds.py
@@ -7,3 +7,4 @@ projects = ['commons-bcel', 'commons-text', 'commons-csv', 'easymock', 'jgit', '
 for prj in projects:
     dft = df.loc[df['project_name'] == prj]
     dft.to_csv('results/ck/' + prj + 'ck_2.csv', index=False)
+    print(prj + ': ' + str(dft.shape))
