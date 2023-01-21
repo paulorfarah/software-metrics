ae380ef4951a1db92dcd74fab7c69cdedaa23a2d
Paulo R. Farah <paulorfarah@gmail.com>
2023-01-21 09:51:56 -0300

    registered number of items of each project

--- static_metrics/split_ds.py
+++ static_metrics/split_ds.py
@@ -7,3 +7,12 @@ projects = ['commons-bcel', 'commons-text', 'commons-csv', 'easymock', 'jgit', '
 for prj in projects:
     dft = df.loc[df['project_name'] == prj]
     dft.to_csv('results/ck/' + prj + 'ck_2.csv', index=False)
+
+
+# commons-bcel: (73868, 87)
+# commons-text: (28502, 87)
+# commons-csv: (2525, 87)
+# easymock: (29713, 87)
+# jgit: (0, 87)
+# gson: (11461, 87)
+# openfire: (0, 87)
