605b0f3718f8e7f804c68adb4e85db4e72ad3106
Paulo R. Farah <paulorfarah@gmail.com>
2023-01-21 09:53:39 -0300

    changed projects list to run again only Openfire

--- static_metrics/split_ds.py
+++ static_metrics/split_ds.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('results/ck/ck_all.csv')
 
-projects = ['commons-bcel', 'commons-text', 'commons-csv', 'easymock', 'jgit', 'gson', 'openfire']
-
+# projects = ['commons-bcel', 'commons-text', 'commons-csv', 'easymock', 'jgit', 'gson', 'Openfire']
+projects = ['Openfire']
 for prj in projects:
     dft = df.loc[df['project_name'] == prj]
     dft.to_csv('results/ck/' + prj + 'ck_2.csv', index=False)
